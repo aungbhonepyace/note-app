76b6250b4e26c52fad5513a603dc9157510845cc
Aung Bhone Pyae <aungbonepyace@gmail.com>
2023-11-02 19:40:33 +0630

    update

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Image style={styles.searchLogo} source={require('./assets/img_search_logo.png')}></Image>
+        <TextInput style={styles.searchBar} placeholder='Search'></TextInput>
+      </View>
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +18,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  searchContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  searchLogo: {
+    width: 30,
+    height: 30
+  },
+  searchBar: {
+    width: 370
+  }
 });
