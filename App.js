315916e48254f5253bd395ecd309fb7b843c2fa2
Aung Bhone Pyae <aungbonepyace@gmail.com>
2023-11-02 20:04:36 +0630

    update

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TextInput, Image } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
   return (
     <SafeAreaView style={styles.container}>
+
+      <View style={styles.mainLogoContainer}>
+        <Text>NOTE</Text>
+        <Text>APP .</Text>
+      </View>
+
       <View style={styles.searchContainer}>
         <Image style={styles.searchLogo} source={require('./assets/img_search_logo.png')}></Image>
-        <TextInput style={styles.searchBar} placeholder='Search'></TextInput>
+        <TextInput style={styles.searchBar} value={text} placeholder='Search for notes'></TextInput>
       </View>
       
+      <Text>hello</Text>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -18,16 +30,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 10,
+  },
+  mainLogoContainer: {
+    flex: 1,
+    fontWeight: 600,
+    fontFamily: 'Inter',
   },
   searchContainer: {
-    flex: 1,
     flexDirection: 'row',
+    borderWidth: 2,
+    padding: 20,
+    alignItems: 'center'
   },
   searchLogo: {
     width: 30,
     height: 30
   },
   searchBar: {
-    width: 370
+    height: 10,
   }
 });
