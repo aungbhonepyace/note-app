77c681e97699eecb2882c78722ba843a477cf2d0
Aung Bhone Pyae <aungbonepyace@gmail.com>
2023-11-07 14:04:45 +0630

    d

--- screen/note/note.js
+++ screen/note/note.js
@@ -1,19 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Image, Button, TouchableOpacity } from 'react-native';
 import React from 'react'
+import { PaperProvider, RadioButton } from 'react-native-paper';
+
 import Header from '../../components/header.js'
 
 const Note = () => {
+  const [checked, setChecked] = useState('');
+
+  const handleRadioPress = (value) => {
+    setChecked(value);
+  };
+
+
   return (
-    <SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView>
         <Header />
-    </SafeAreaView>
+        <View>
+          <Text style={note_styles.title}>Title</Text>
+          <TextInput style={note_styles.titleInputfield} placeholder='Enter Title' />
+        </View>
+
+        <View>
+          <Text>Category</Text>
+          <RadioButton.Group onValueChange={handleRadioPress} value={checked}>
+            <RadioButton.Item status={ checked === 'first' ? 'checked' : 'unchecked' } label="First" value="first" />
+            <RadioButton.Item status={ checked === 'first' ? 'checked' : 'unchecked' } label="Second" value="second" />
+            <RadioButton.Item status={ checked === 'first' ? 'checked' : 'unchecked' }label="Third" value="third" />
+          </RadioButton.Group>
+          <Text>Selected option: {checked}</Text>
+
+          
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
 
     
   )
 }
 
 import { note_styles } from './note.style'
-import { home_styles } from '../home/home.style';
+//import { home_styles } from '../home/home.style';
 export default Note
